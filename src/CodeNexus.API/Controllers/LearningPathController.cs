58fb6b0d8895f5536b1c80f0bd4ba325e8b8b2b8
jayptl11 <phamlong2k3@gmail.com>
2026-02-12 20:30:26 +0700

    add api for test

--- src/CodeNexus.API/Controllers/LearningPathController.cs
+++ src/CodeNexus.API/Controllers/LearningPathController.cs
@@ -1,6 +1,8 @@
 using CodeNexus.Application.Common.Models;
+using CodeNexus.Application.Features.Chapters.Commands.GenerateChapterContent;
 using CodeNexus.Application.Features.LearningPaths.Commands.GenerateLearningPathSkeleton;
 using CodeNexus.Application.Features.Lessons.Commands.GenerateLessonContent;
+using CodeNexus.Application.Features.Quizzes.Commands.GenerateQuizQuestions;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -36,6 +38,20 @@ public class LearningPathController : ControllerBase
     public async Task<IActionResult> GenerateLessonContent(Guid lessonId, CancellationToken cancellationToken)
     {
         var result = await _mediator.Send(new GenerateLessonContentCommand(lessonId), cancellationToken);
+        return ToActionResult(result);
+    }
+
+    [HttpPost("chapters/{chapterId:guid}/generate-content")]
+    public async Task<IActionResult> GenerateChapterContent(Guid chapterId, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GenerateChapterContentCommand(chapterId), cancellationToken);
+        return ToActionResult(result);
+    }
+
+    [HttpPost("quizzes/{quizId:guid}/generate-questions")]
+    public async Task<IActionResult> GenerateQuizQuestions(Guid quizId, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new GenerateQuizQuestionsCommand(quizId), cancellationToken);
         return ToActionResult(result);
     }
 
