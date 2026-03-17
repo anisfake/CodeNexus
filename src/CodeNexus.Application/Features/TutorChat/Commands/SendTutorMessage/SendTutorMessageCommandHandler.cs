1e35a7ddfc505558d4fce2153397173c895babee
anisfake <ngoquocan07122003@gmail.com>
2026-03-17 22:43:57 +0700

    Update SendTutorMessageCommandHandler.cs

--- src/CodeNexus.Application/Features/TutorChat/Commands/SendTutorMessage/SendTutorMessageCommandHandler.cs
+++ src/CodeNexus.Application/Features/TutorChat/Commands/SendTutorMessage/SendTutorMessageCommandHandler.cs
@@ -198,6 +198,7 @@ public class SendTutorMessageCommandHandler : IRequestHandler<SendTutorMessageCo
 
             chapter = lesson.Chapter;
             learningPath = chapter.LearningPath;
+        }
         else if (chapterId.HasValue)
         {
             chapter = await _context.Chapters
@@ -212,6 +213,7 @@ public class SendTutorMessageCommandHandler : IRequestHandler<SendTutorMessageCo
                 return Result<TutorContext>.Failure("CHAPTER_NOT_FOUND", "Chapter not found");
 
             learningPath = chapter.LearningPath;
+        }
         else if (learningPathId.HasValue)
         {
             learningPath = await _context.LearningPaths
