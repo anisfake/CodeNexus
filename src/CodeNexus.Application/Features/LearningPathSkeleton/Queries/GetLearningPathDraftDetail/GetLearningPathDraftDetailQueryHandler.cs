6a29521313ba4d23068df4557d35a30c4dd2a4ea
jayptl11 <phamlong2k3@gmail.com>
2026-04-08 23:22:43 +0700

    update filter question

--- src/CodeNexus.Application/Features/LearningPathSkeleton/Queries/GetLearningPathDraftDetail/GetLearningPathDraftDetailQueryHandler.cs
+++ src/CodeNexus.Application/Features/LearningPathSkeleton/Queries/GetLearningPathDraftDetail/GetLearningPathDraftDetailQueryHandler.cs
@@ -54,7 +54,7 @@ public class GetLearningPathDraftDetailQueryHandler : IRequestHandler<GetLearnin
             .Include(lp => lp.Chapters.Where(c => !c.IsDeleted))
                 .ThenInclude(c => c.Lessons.Where(l => !l.IsDeleted))
                 .ThenInclude(l => l.Quizzes.Where(q => !q.IsDeleted))
-                .ThenInclude(q => q.Questions)
+                .ThenInclude(q => q.Questions.Where(qq => !qq.IsDeleted))
             .Include(lp => lp.Chapters.Where(c => !c.IsDeleted))
                 .ThenInclude(c => c.Tasks.Where(t => !t.IsDeleted))
             .FirstOrDefaultAsync(lp => lp.PathId == request.PathId, cancellationToken);
@@ -111,6 +111,7 @@ public class GetLearningPathDraftDetailQueryHandler : IRequestHandler<GetLearnin
                         q.Title,
                         q.Description,
                         q.Questions
+                            .Where(qq => !qq.IsDeleted)
                             .OrderBy(qq => qq.OrderIndex ?? int.MaxValue)
                             .Select(qq => new QuestionDto(
                                 qq.QuestionId,
